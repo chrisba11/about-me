bc009bb1ab772cf160d79d3381a0d75b070def96
Chris Ball <chrisball7@gmail.com>
2018-11-28 20:26:27 -0800

    removed comment from js, added console logging for number of correct answers

--- js/app.js
+++ js/app.js
@@ -222,12 +222,17 @@ while (guessesRemainingTwo > 0) {
 
 if(numberCorrect === 0) {
   alert('Sorry ' + userName + ', you really don\'t know me at all!');
+  console.log('Sorry ' + userName + ', you really don\'t know me at all!');
 } else if (numberCorrect > 0 && numberCorrect < 4) {
   alert(userName + ', you got ' + numberCorrect + '/7 answers correct. Not terrible, but not good.');
+  console.log(userName + ', you got ' + numberCorrect + '/7 answers correct. Not terrible, but not good.')
 } else if (numberCorrect > 3 && numberCorrect < 6) {
   alert(userName + ', you got ' + numberCorrect + '/7 answers correct. Not bad, but not great.');
+  console.log(userName + ', you got ' + numberCorrect + '/7 answers correct. Not bad, but not great.')
 } else if (numberCorrect > 5 && numberCorrect < 7) {
   alert('You got ' + numberCorrect + '/7 answers correct! That\'s pretty darn great!.');
+  console.log('You got ' + numberCorrect + '/7 answers correct! That\'s pretty darn great!.')
 } else {
   alert(userName + ' is a genius! PERFECT SCORE!!!');
+  console.log(userName + ' is a genius! PERFECT SCORE!!!')
 }
